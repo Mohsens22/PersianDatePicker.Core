505fe87bbb3b663a7b6affdd3b9ef2f3dd7f01bc
Ali Jebali <jebaleali@gmail.com>
2024-05-03 19:09:46 +0330

    upgrade to .NET8 and removed Unused namespaces

--- src/SampleProject/MainWindow.xaml.cs
+++ src/SampleProject/MainWindow.xaml.cs
@@ -1,38 +1,19 @@
 ﻿using Mohsen;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace SampleProject
+namespace SampleProject;
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            this.datePicker.DisplayDateStart = this.calendar.DisplayDateStart =
-                (new PersianDate(1, 1, 1)).ToDateTime();
-            this.persianDatePicker.DisplayDateEnd = this.persianCalendar.DisplayDateEnd = new PersianDate(DateTime.MaxValue);
-            this.datePicker.DisplayDateEnd = this.calendar.DisplayDateEnd = DateTime.MaxValue;
-
-            
-        }
+        InitializeComponent();
+    }
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        this.datePicker.DisplayDateStart = this.calendar.DisplayDateStart =
+            (new PersianDate(1, 1, 1)).ToDateTime();
+        this.persianDatePicker.DisplayDateEnd = this.persianCalendar.DisplayDateEnd = new PersianDate(DateTime.MaxValue);
+        this.datePicker.DisplayDateEnd = this.calendar.DisplayDateEnd = DateTime.MaxValue;
     }
 }
